e2ba8d7812d23f7f6ef9c542cac3c1a6c48aaea4
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-17 19:11:41 -0400

    Remove: removed demo code

--- src/App.js
+++ src/App.js
@@ -15,14 +15,6 @@ class App extends Component {
             <Route path="/login" component={Login} />
           </div>
         </Router>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Login/>
       </div>
     );
   }
