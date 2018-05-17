cc3658c5fc1e5ba4fe7815986c902c2a71ef6a8b
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-17 19:09:57 -0400

    Add: Router logic for link to login screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
+import Login from './login';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
+          <div>
+            <Link to="/login">Login</Link>
+            <Route path="/login" component={Login} />
+          </div>
+        </Router>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,6 +22,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Login/>
       </div>
     );
   }
