d95962fb80d39d0f20ef0cadc963ec69938609b3
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-17 19:19:53 -0400

    Add: class for styles

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import Login from './login';
+import Register from './register';
 import './App.css';
 
 const MyRouter = () => (
   <Router>
-    <div>
+    <div className="nav-links">
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
 
       <hr />
 
       <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </div>
   </Router>
 )
