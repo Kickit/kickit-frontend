d9b4812530cdf20c556a373232e50cf4b7653c7b
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-17 22:27:39 -0400

    Add: Regiter modal step one

--- src/components/register.js
+++ src/components/register.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import '../index.css';
+import { Button, Input } from 'semantic-ui-react'
+
+const RegEmail = () => {
+  return (
+    <div className="container">
+    <Input placeholder='Email' />
+    <Button color='yellow'>Register</Button>
+    </div>
+  )
+}
+
 
 const Register = () => {
-    return (<div className="register">Register Screen!</div>);
+    return (
+    <div className="register">
+      <div className="inputModal">
+        <h3>Register</h3>
+        <RegEmail/>
+      </div>
+    </div>
+  );
   };
   
   export default Register;
