69b6bed25b977da99a827df9ee7f83a0af4f52fc
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-17 22:47:34 -0400

    Add: circle background css

--- src/App.js
+++ src/App.js
@@ -27,6 +27,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="circle one">&nbsp;</div>
+        <div className="circle two">&nbsp;</div>
+        <div className="circle three">&nbsp;</div>
+        <div className="circle four">&nbsp;</div>
+        <div className="circle five">&nbsp;</div>
+        <div className="circle six">&nbsp;</div>
+        <div className="circle seven">&nbsp;</div>
+        <div className="circle eight">&nbsp;</div>
+        <div className="circle nine">&nbsp;</div>
+        <div className="circle ten">&nbsp;</div>
+        <div className="circle eleven">&nbsp;</div>
+        <div className="circle twelve">&nbsp;</div>
+        <div className="circle thirteen">&nbsp;</div>
+        <div className="circle fourteen">&nbsp;</div>
         <MyRouter/>
       </div>
     );
