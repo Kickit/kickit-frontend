db6b59b9048c39992a50fa8e699da26f15c61128
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-19 13:56:11 -0400

    Add: login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Button, Input } from 'semantic-ui-react'
+import logo from '../kickit_logo.png';
+
 import '../index.css';
 import 'semantic-ui-css/semantic.min.css';
 
+const LoginModal = () => {
+  return (
+    <div className="container">
+    <h3>Login</h3>
+    <Input placeholder='Email' />
+    <Input placeholder='Password' />
+    <Button color='yellow'>Login</Button>
+    </div>
+  )
+}
+
+
 const Login = () => {
-    return (<div className="login">Login Screen!</div>);
+    return (
+      <div className="login">
+        <img src={logo} height={"300px"} width={"300px"} alt={""}/>
+        <div className="inputModal">
+          <LoginModal/>
+        </div>
+      </div>
+    );
   };
   
   export default Login;
