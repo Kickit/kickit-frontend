16d538b18ee376dd893555145ed1dc749a7e908b
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-25 21:14:07 -0400

    Fix: linting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './App.css'
 
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
