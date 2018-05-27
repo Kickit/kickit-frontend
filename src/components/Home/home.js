f1c0cb4cb100fb2c4866d6a57540323fff2b38ae
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-05-26 21:16:40 -0400

    Add: shell for home route

---
+++ src/components/Home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../index.css';
+import { Button, Input, Form, Message } from 'semantic-ui-react'
+import logo from '../../kickit_logo.png';
+
+
+
+class Home extends React.Component {
+    render () {
+        return 'this is the home route'
+    }
+       
+}
+
+export default Home
