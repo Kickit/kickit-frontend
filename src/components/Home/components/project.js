e41baaeb3da1e2909cd87c331ad4c04acf685f86
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-03 17:49:00 -0400

    Add: sortable list component

--- src/components/Home/components/project.js
+++ src/components/Home/components/project.js
@@ -6,30 +6,67 @@ import {
 	Header, Label, Divider 
 } from 'semantic-ui-react'
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 import styled, { css } from 'styled-components'
 import data from '../../../fixture'
 import '../../../index.css'
 
 
+const SortableItem = SortableElement(({value}) =>
+    {if(value.type == 'section'){
+        return <li><h1>{value.data.title}</h1></li>
+    } else {
+        return <li>{value.data.title}</li>
+    }}
+)
+
+const SortableList = SortableContainer(({items}) => {
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${index}`} index={index} value={value} />
+      ))}
+    </ul>
+  )
+})
+
+
 class Project extends React.Component {
 	constructor(props) {
         super(props)
+        let items = []
+        if(props.project){
+            items = [].concat(...props.project.sections.map( section => {
+                return [].concat({type: 'section', data: section}, ...section.tasks.map( task => {
+                    return {type:'task', data: task}
+                }))
+            }))
+        }
+        console.log(items)
         this.state = {
-            project: props.project
+            project: props.project,
+            items: items
         }
     }
 
     componentWillReceiveProps(nextProps) {
         this.setState({ project: nextProps.project });  
     }
+
+    onSortEnd = ({oldIndex, newIndex}) => {
+        this.setState({
+            items : arrayMove(this.state.items, oldIndex, newIndex)
+        })
+
+    }
 	render() {
+        console.log(this.state.items)
 		return (
 			<div>
-            {this.state.project.id}
+                <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
 		    </div>
 		)
 	}
-
 }
 
 export default Project
