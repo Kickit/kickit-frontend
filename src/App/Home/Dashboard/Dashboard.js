bcdc9515a3707670022127190cea6485862161b5
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-10 12:56:11 -0400

    Add: dashboard shell

---
+++ src/App/Home/Dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// Dashboard: Dashboard style setup with some grouping and visualizations
+class Dashboard extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			data: props.data,
+		}
+    }
+    
+	render() {
+		return (
+			<div>
+                Dashboard
+            </div>
+		)
+	}
+
+}
+export default Dashboard
