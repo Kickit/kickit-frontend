a551c00a14e71533559e7b6d5a4a46f8ba7a4b3e
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-15 16:30:01 -0400

    Add: project.js will only contain the list of projects

--- src/App/Home/Project/Project.js
+++ src/App/Home/Project/Project.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Icon } from 'semantic-ui-react'
 import ProjectList from './components/ProjectList'
-import { Card, Row, Column, media} from '../../../utils/anvil'
+import { Column, media} from '../../../utils/anvil'
 import { POLL_INTERVAL } from '../../../utils/constants'
 import { project } from '../../../graphql/queries'
 import { graphql } from 'react-apollo'
@@ -17,35 +16,13 @@ const KickitList = graphql(project, {
   })
 })(ProjectList)
 
-// TaskDetails: Full information related to the task 
-const TaskDetails = ({task, close}) => {
-	if (task) {
-		return (
-			<Card>
-				<Row><Icon name='close' onClick={()=> close(null)}/></Row>
-				<h1>{task.data.title}</h1>
-				<p>{task.data.description}</p>
-			</Card>
-		)
-	} else {
-		return null
-	}
-}
-
 // Project: Component used on the /projects/:id route
-class Project extends React.Component {	
+class Project extends React.Component {
 	render() {
 		return (
-			<Row>
-				<ListColumn style={{display: this.state.selectedItem ? '' : 'inherit'}}>
-					<KickitList id={this.props.match.params.projectid} location={this.props.location.pathname} />
-				</ListColumn>
-				<DetailColumn style={{display: this.state.selectedItem ? '' : 'none'}}>
-					<TaskDetails 
-						close={e => this.selectItem(e)} 
-						task={this.state.selectedItem}/>
-				</DetailColumn>
-		    </Row>
+			<ListColumn style={{display: this.props.match.params.taskid ? '' : 'inherit'}}>
+				<KickitList id={this.props.match.params.projectid} location={this.props.location.pathname} />
+			</ListColumn>
 		)
 	}
 }
@@ -55,9 +32,5 @@ const ListColumn = Column.extend`
     flex: 1;
     ${media.phone`display: none;`}
 `
-const DetailColumn = Column.extend`
-    flex: 1;
-`
-
 
 export default Project
