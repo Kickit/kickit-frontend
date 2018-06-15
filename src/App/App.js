636d9a3d87f2c1a9a187cb2b5801159e4e1d40c0
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-15 15:54:04 -0400

    Add: background component

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Background from './components/background'
 import Login from './Login/Login'
 import Home from './Home/Home'
 import '../styles/App.css'
+import Background from './components/background';
 
 class App extends React.Component {
 	render() {
@@ -16,19 +18,7 @@ class App extends React.Component {
 					</Switch>
 				</Router>
 				{/* Todo: make this a component */}
-				<div className="circle one">&nbsp;</div>
-				<div className="circle three">&nbsp;</div>
-				<div className="circle four">&nbsp;</div>
-				<div className="circle five">&nbsp;</div>
-				<div className="circle six">&nbsp;</div>
-				<div className="circle seven">&nbsp;</div>
-				<div className="circle eight">&nbsp;</div>
-				<div className="circle nine">&nbsp;</div>
-				<div className="circle ten">&nbsp;</div>
-				<div className="circle eleven">&nbsp;</div>
-				<div className="circle twelve">&nbsp;</div>
-				<div className="circle thirteen">&nbsp;</div>
-				<div className="circle fourteen">&nbsp;</div>
+				<Background/>
 			</div>
 		)
 	}
---
+++ src/App/components/background.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class Background extends React.Component {
+	render() {
+		return (
+			<div>
+				<div className="circle one">&nbsp;</div>
+				<div className="circle three">&nbsp;</div>
+				<div className="circle four">&nbsp;</div>
+				<div className="circle five">&nbsp;</div>
+				<div className="circle six">&nbsp;</div>
+				<div className="circle seven">&nbsp;</div>
+				<div className="circle eight">&nbsp;</div>
+				<div className="circle nine">&nbsp;</div>
+				<div className="circle ten">&nbsp;</div>
+				<div className="circle eleven">&nbsp;</div>
+				<div className="circle twelve">&nbsp;</div>
+				<div className="circle thirteen">&nbsp;</div>
+				<div className="circle fourteen">&nbsp;</div>
+			</div>
+		)
+	}
+}
+
+export default Background
