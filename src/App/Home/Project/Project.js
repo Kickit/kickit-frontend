e54b565b7ef2b7e2f49fa73f9c2cec3bb2acdb39
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-16 12:29:55 -0400

    Fix: reactive for mobile

--- src/App/Home/Project/Project.js
+++ src/App/Home/Project/Project.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import ProjectList from './components/ProjectList'
-import { ListColumn } from '../../../utils/anvil'
+import QueryList from './components/QueryList'
+import { CardColumn } from '../../../utils/anvil'
 import { POLL_INTERVAL } from '../../../utils/constants'
 import { project } from '../../../graphql/queries'
 import { graphql } from 'react-apollo'
@@ -14,15 +14,15 @@ const KickitList = graphql(project, {
 		},
 		pollInterval: POLL_INTERVAL,
   })
-})(ProjectList)
+})(QueryList)
 
 // Project: Component used on the /projects/:id route
 class Project extends React.Component {
 	render() {
 		return (
-			<ListColumn style={{display: this.props.match.params.taskid ? '' : 'inherit'}}>
+			<CardColumn>
 				<KickitList id={this.props.match.params.projectid} location={this.props.location.pathname} />
-			</ListColumn>
+			</CardColumn>
 		)
 	}
 }
