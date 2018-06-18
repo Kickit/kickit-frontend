a7c50e2029f6360a844597496f0d33f1c215eae8
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-18 17:19:25 -0400

    Add: break out error into stateless component

---
+++ src/App/Login/components/ErrorModal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, Input, Form, Message } from 'semantic-ui-react'
+
+  //Generic Error 'message' component
+export default (error) => {
+    if(error.error !== ""){
+      return (
+        <Message warning>
+          <Message.Header>Opps!</Message.Header>
+          <p>{error.error}</p>
+        </Message>
+      )
+    }
+    return ''
+}
