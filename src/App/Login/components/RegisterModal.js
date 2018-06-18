5dc15513012047ab3bd88938aefbdda0a6616410
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-18 17:19:12 -0400

    Add: break out registration into stateless component

---
+++ src/App/Login/components/RegisterModal.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Button, Input, Form, Message } from 'semantic-ui-react'
+
+
+// RegistrationModal: Registration form
+export default ({ state, handleChange, updateStep, attemptRegister }) => {
+    switch(state.step) {
+      case 1:
+        return (
+          <div className="container">
+          <h3>Register</h3>
+          <Form>
+            <Form.Field>
+              <Input 
+                value={state.email} 
+                onChange={ handleChange } 
+                placeholder='Email'
+                name='email' 
+              />
+            </Form.Field>
+            <Form.Field>
+              <Button 
+                onClick={() => updateStep(1)} 
+                color='yellow'>Next {'>'}</Button>
+            </Form.Field>
+          </Form>
+          </div>
+        )
+      case 2:
+        return (
+          <div className="container">
+          <h3>Register</h3>
+          <Form>
+            <Form.Field>
+              <Input 
+                value={state.first} 
+                onChange={ handleChange } 
+                placeholder='First Name' 
+                name='first'
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input 
+                value={ state.last } 
+                onChange={ handleChange } 
+                placeholder='Last Name'
+                name='last'
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input 
+                value={state.password} 
+                onChange={ handleChange } 
+                placeholder='Password' type='password' 
+                name='password'
+              />
+            </Form.Field>
+            <Form.Field>
+              <Button 
+                onClick={() => updateStep(-1)} 
+                color='blue'> 
+                {'<'} Back
+              </Button>
+              <Button 
+                type='submit' 
+                onClick={() => attemptRegister()} 
+                color='yellow'>Register</Button>
+            </Form.Field>
+          </Form>
+          </div>
+        )
+      case 3:
+        return (
+          <div className="container">
+          <h3>Thankyou for Registering!</h3>
+          <a> If you are not redirected to application click here</a>
+          
+          </div>
+        )
+    }
+}
