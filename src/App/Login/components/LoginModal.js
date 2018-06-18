79fa7bea6f5c4b2903095adc3e71739e635756e0
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-18 17:19:45 -0400

    Add: break out login into stateless component

---
+++ src/App/Login/components/LoginModal.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button, Input, Form, Message } from 'semantic-ui-react'
+
+//2 input boxes for email and password and a button 
+export default ({ state, handleChange, attemptLogin }) => {
+    return (
+      <div className="container">
+      <h3>Login</h3>
+      <Form>
+        <Form.Field>
+          <Input 
+            value={state.email}
+            onChange={ handleChange }
+            placeholder='Email'
+            name= 'email'/>
+        </Form.Field>
+        <Form.Field>
+          <Input 
+            value={ state.password } 
+            onChange={ handleChange } 
+            placeholder='Password' 
+            type='password'
+            name= 'password'/>
+        </Form.Field>
+        <Form.Field>
+          <Button 
+            type='submit' 
+            onClick={() => attemptLogin()}  
+            color='yellow'>Login</Button>
+        </Form.Field>
+      </Form>
+      </div>
+    )
+}
