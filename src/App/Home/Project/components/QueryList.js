304909f782bebf6315d92f02ee3a3ca49ae37207
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-23 14:08:41 -0400

    Add: massive cleanup of Query list

--- src/App/Home/Project/components/QueryList.js
+++ src/App/Home/Project/components/QueryList.js
@@ -8,70 +8,50 @@ import '../../../../styles/index.css'
 
 // Project: Component used on the /projects/:id route
 class QueryList extends React.Component {
-	state = {
-		project: null,
-		items: [],
-		selectedItem: null,
-	}
-
-	shouldComponentUpdate(nextProps) {
-		if (nextProps.data && nextProps.data.project && nextProps.data.project !== this.state.project) {
-			this.setState({ 
-				project: nextProps.data.project, 
-				items: this.projectItems(nextProps.data.project) 
-			})  	
-		}
-		return true
-	}
-
 	projectItems(project) {
-		if( !project ){return []}
-		return [].concat(...project.sections.map( section => {
-			return [].concat({type: 'section', data: section}, ...section.tasks.map( task => {
-					return {type:'task', data: task}
+		if (!project) { return [] }
+		return [].concat(...project.sections.map(section => {
+			return [].concat({ type: 'section', data: section }, ...section.tasks.map(task => {
+				return { type: 'task', data: task }
 			}))
 		}))
 	}
 
-	selectItem(item) {
-		this.setState({selectedItem: item})
-	}
-
-	onSortEnd = ({oldIndex, newIndex}) => {
+	onSortEnd = ({ oldIndex, newIndex }) => {
 		this.setState({
-			items : arrayMove(this.state.items, oldIndex, newIndex)
+			items: arrayMove(this.state.items, oldIndex, newIndex)
 		})
 	}
-
+	// TODO: @nicklewanowicz NEED to clean this garbage up
 	listItem = ({ value }) => (
-		<div onClick={()=> this.selectItem(value)}>
+		<div onClick={() => this.props.selectItem(value)}>
 			<Link to={`/0/projects/${this.props.match.params.projectid}/tasks/${value.data.id}`} >
-			<ListItem className={`item ${value.type === 'section' ? 'section' : 'task'}`}>
-				{value.type === 'task' &&
-					<EditableItem value={value} updateTask={(options) => this.props.updateTask(options)}/>
-				}
-				{value.type === 'section' &&
-					<span>{value.data.title}</span>
-				}
-			</ListItem>
+				<ListItem className={`item ${value.type === 'section' ? 'section' : 'task'}`}>
+					{value.type === 'task' &&
+						<EditableItem value={value} onChange={this.props.onChange} />
+					}
+					{value.type === 'section' &&
+						<span>{value.data.title}</span>
+					}
+				</ListItem>
 			</Link>
 		</div>
 	)
-	
-	render() {
-	
-	if (this.props.data.loading) {
-      return (<div>Loading</div>)
-    }
 
-    if (this.props.data.error) {
-      console.log(this.props.data.error)
-      return (<div>An unexpected error occurred</div>)
-    }
-    return (
-			<KickitList 
+	render() {
+
+		if (this.props.data.loading) {
+			return (<div>Loading</div>)
+		}
+
+		if (this.props.data.error) {
+			console.log(this.props.data.error)
+			return (<div>An unexpected error occurred</div>)
+		}
+		return (
+			<KickitList
 				itemTemplate={this.listItem}
-				items={this.state.items} 
+				items={this.projectItems(this.props.data.project)}
 				onSortEnd={this.onSortEnd}
 			/>
 		)
