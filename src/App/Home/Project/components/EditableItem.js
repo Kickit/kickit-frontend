d9e763271c7200ac92b89451c728bb9ab685f1f9
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-25 20:45:33 -0400

    Add: update editable item to use editable field not slate field

--- src/App/Home/Project/components/EditableItem.js
+++ src/App/Home/Project/components/EditableItem.js
@@ -1,42 +1,27 @@
 import React, { Component } from 'react'
-import Plain from 'slate-plain-serializer'
-import {debounce} from 'throttle-debounce';
-// import { Link } from 'react-router-dom'
-// import { Icon } from 'semantic-ui-react'
-import { TextEditor, Row } from '../../../../utils/anvil'
+import { Row } from '../../../../utils/anvil'
+import EditableField from '../../../components/EditableField';
 
 class EditableItem extends Component {
-  state = {
-		title: Plain.deserialize(this.props.value.data.title),
-		description: Plain.deserialize(this.props.value.data.description || ''),
-		id: this.props.value.data.id,
-	}
-
-  onChange = ({value, name}) => {
-		this.setState({ [name]: value })
-		debounce(1000, this.props.updateTask({
-			variables: {
-				id: this.state.id,
-				[name]: Plain.serialize(value)
-			}
-		}))
-	}
 	render() {
     return (
       <Row>
-        <TextEditor
-          value={this.state.title}
-          onChange={(e) => this.onChange({...e, name: 'title'})}
+        <EditableField
+					field='title'
+          value={this.props.item.data.title}
+					onChange={this.props.onChange}
+					
         />
-				{!this.props.value.data.tasks &&
-					<TextEditor
-						value={this.state.description}
-						onChange={(e) => this.onChange({...e, name: 'description'})}
+				{this.props.item.data.description &&
+					<EditableField
+					field='description'
+						value={this.props.item.data.description}
+						onChange={this.props.onChange}
+						
 					/>
 				}
       </Row>
     )
 	}
 }
-
 export default EditableItem
