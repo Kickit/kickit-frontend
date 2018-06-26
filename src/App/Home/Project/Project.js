4f3b9788b7b9602762ea66b8514d51de88ca121d
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-25 20:33:13 -0400

    Add: project route now controls data going into list and item details

--- src/App/Home/Project/Project.js
+++ src/App/Home/Project/Project.js
@@ -1,34 +1,91 @@
 import React from 'react'
 import QueryList from './components/QueryList'
-import { CardColumn } from '../../../utils/anvil'
-import { POLL_INTERVAL } from '../../../utils/constants'
+import { CardColumn, Row, Card } from '../../../utils/anvil'
+import EditableTask from './components/EditableTask'
 import { project } from '../../../graphql/queries'
 import { updateTask } from '../../../graphql/mutations'
 import { graphql, compose } from 'react-apollo'
 
 import '../../../styles/index.css'
 
-const KickitList = compose(
-	graphql(project, {
-		options: (ownProps) => ({
-			variables: {
-				id: ownProps.id
-			},
-			pollInterval: POLL_INTERVAL,
-		})
-	}),
-	graphql(updateTask, { name: 'updateTask'})
-)(QueryList)
-
 // Project: Component used on the /projects/:id route
 class Project extends React.Component {
+	state = {
+		selectedItem: null,
+		items: []
+	}
+	shouldComponentUpdate(nextProps, nextState) {
+		if (nextProps.data.project) {
+			// if ( nextState.selectedItem && Object.keys(nextState.selectedItem) == 0) { nextState.selectedItem = this.state.selectedItem }
+			nextState.items = this.getItems(nextProps.data.project)
+		}
+		return true
+	}
+	selectItem = (value) => {
+		this.setState({selectedItem: value})
+	}
+
+	getItems(project) {
+		if (!project) { return [] }
+		return [].concat(...project.sections.map(section => {
+			return [].concat({ type: 'section', data: section }, ...section.tasks.map(task => {
+				return { type: 'task', data: task }
+			}))
+		}))
+	}
+
+	onChange = ({key, value}) => {
+		let selectedItem = this.state.selectedItem
+		if(!selectedItem || selectedItem.data[key] == value) { return }
+		selectedItem.data = {...selectedItem.data, [key]: value}
+		this.setState({ selectedItem })
+		this.props.updateTask({
+			variables: {
+				id: selectedItem.data.id,
+				[key]: value
+			}
+		})
+	}
+
 	render() {
+		if (this.props.data.loading) {
+			return (<div>Loading</div>)
+		}
+
+		if (this.props.data.error) {
+			console.log(this.props.data.error)
+			return (<div>An unexpected error occurred</div>)
+		}
 		return (
+			<Row>
 			<CardColumn>
-				<KickitList id={this.props.match.params.projectid} location={this.props.location.pathname} />
+				<QueryList 
+					items={this.state.items}
+					selectedItem={this.state.selectedItem}
+					selectItem={this.selectItem}
+					onChange={this.onChange} />
 			</CardColumn>
+			{this.state.selectedItem &&
+			<CardColumn>
+				<Card>
+					<EditableTask 
+						item={this.state.selectedItem} 
+						onChange={this.onChange} 
+						location={this.props.location.pathname} />
+				</Card>
+			</CardColumn>}
+			</Row>
 		)
 	}
 }
 
-export default Project
+export default compose(
+	graphql(project, {
+		options: (props) => ({
+			variables: {
+				id: props.match.params.projectid
+			}
+		})
+	}),
+	graphql(updateTask, { name: 'updateTask'})
+)(Project)
