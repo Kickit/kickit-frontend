93190761fc3e8a90e87045be4e938ec6a520d812
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-25 20:36:24 -0400

    Add: generic editable field component

---
+++ src/App/components/EditableField.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Plain from 'slate-plain-serializer'
+import { Editor } from 'slate-react'
+
+class EditableField extends Component {
+    state = {
+        value: Plain.deserialize(this.props.value || ''),
+    }
+    
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.value !== Plain.serialize(nextState.value)){
+            nextState.value = Plain.deserialize(nextProps.value)
+        }
+        return true
+    }
+    onChange = ({value}) => {
+        this.setState({value})
+        this.props.onChange({
+            value: Plain.serialize(value),
+            key: this.props.field,
+        })
+    }
+	render() {
+        return (
+            <Editor
+                value={this.state.value}
+                onChange={this.onChange}
+            />
+        )
+	}
+}
+
+export default EditableField
