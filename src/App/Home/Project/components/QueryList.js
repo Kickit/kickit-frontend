ebc557170eaf76cd7c50ec701efd62b90ddd1fb8
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-06-25 20:46:40 -0400

    Add: overhaul on QueryList and removed list

--- src/App/Home/Project/components/QueryList.js
+++ src/App/Home/Project/components/QueryList.js
@@ -1,61 +1,50 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import { arrayMove } from 'react-sortable-hoc'
-import { ListItem } from '../../../../utils/anvil'
-import KickitList from '../../../components/list'
-import EditableItem from '../components/EditableItem'
+import { Card, Row, ListItem } from '../../../../utils/anvil'
+import EditableItem from './EditableItem';
+
 import '../../../../styles/index.css'
 
 // Project: Component used on the /projects/:id route
 class QueryList extends React.Component {
-	projectItems(project) {
-		if (!project) { return [] }
-		return [].concat(...project.sections.map(section => {
-			return [].concat({ type: 'section', data: section }, ...section.tasks.map(task => {
-				return { type: 'task', data: task }
-			}))
-		}))
-	}
-
-	onSortEnd = ({ oldIndex, newIndex }) => {
-		this.setState({
-			items: arrayMove(this.state.items, oldIndex, newIndex)
-		})
-	}
 	// TODO: @nicklewanowicz NEED to clean this garbage up
-	listItem = ({ value }) => (
+	listItem = ({ value }) => {
+		return (
 		<div onClick={() => this.props.selectItem(value)}>
 			<Link to={`/0/projects/${this.props.match.params.projectid}/tasks/${value.data.id}`} >
 				<ListItem className={`item ${value.type === 'section' ? 'section' : 'task'}`}>
-					{value.type === 'task' &&
-						<EditableItem value={value} onChange={this.props.onChange} />
-					}
-					{value.type === 'section' &&
-						<span>{value.data.title}</span>
-					}
+					<Row>
+						<EditableItem
+							field='title'
+							item={value}
+							onChange={this.props.onChange} />
+					</Row>
 				</ListItem>
 			</Link>
 		</div>
-	)
+		)
+	}
 
 	render() {
-
-		if (this.props.data.loading) {
-			return (<div>Loading</div>)
-		}
-
-		if (this.props.data.error) {
-			console.log(this.props.data.error)
-			return (<div>An unexpected error occurred</div>)
-		}
 		return (
-			<KickitList
-				itemTemplate={this.listItem}
-				items={this.projectItems(this.props.data.project)}
-				onSortEnd={this.onSortEnd}
-			/>
+			<ProjectList>
+				{this.props.items.map(item => (
+					<this.listItem key={item.data.id} value={item}/>
+				))}
+			</ProjectList>
 		)
 	}
 }
 
 export default withRouter(QueryList)
+
+// TODO: Make this more generic and add to anvil
+const ProjectList = Card.extend`
+    &.ui.segment {
+        background: linear-gradient(135deg, rgba(30, 187, 202,0.4), rgba(235, 188, 167, 0.4));
+        border-radius: 0.5rem;
+        color: #FFFFFF;
+        list-style: none;
+        overflow-y: overlay;
+    }
+`
