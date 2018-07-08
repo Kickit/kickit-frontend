745f99a970fe08f076fec7a84f8d0af03d4824ff
Foopert <foopert@gmail.com>
2018-07-08 15:03:30 -0400

    FIX: Fixed fixtures to have better mock data

--- src/utils/fixture.js
+++ src/utils/fixture.js
@@ -6,42 +6,42 @@ export default {
 	'projects':[
 		{
 			'id':'5b1d81bab14c2e6a96b1ac1c',
-			'title':'framework',
+			'title':'Coding project - Python data entry bot',
 			'created':1527611974,
 			'sections':[
 				{
 					'id':'0ca3f769-fe79-4da4-ae42-369108d3241a',
-					'title':'standardization',
+					'title':'Initialize Python',
 					'position':1,
 					'tasks':[
 						{ 
 							'id':'102de32c-14ca-4ef2-9850-a3aaa9c8fc2b',
-							'title':'heuristic',
-							'description':'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum.',
+							'title':'Find tutorial online',
+							'description':'Ill need to find a good python tutorial/textbook online in order for me to automate my very first Python bot. https://automatetheboringstuff.com/ looks like a good start?',
 							'created':1527720204,
 							'due':1527645240,
 							'completed':false
 						},
 						{
 							'id':'094d6deb-56a3-4e6d-930f-5bf4973836f6',
-							'title':'Ergonomic',
-							'description':'Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+							'title':'Read through tutorial',
+							'description':'Cant start coding until I read atleast the first couple chapters of the tutorial',
 							'created':1527703020,
 							'due':1527652850,
 							'completed':false
 						},
 						{
 							'id':'1c4be7bb-e058-4b66-915e-572799f0309f',
-							'title':'Operative',
-							'description':'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio.',
+							'title':'Setup Python dependencies',
+							'description':'Setup necessary python files with homebrew on local machine',
 							'created':1527671122,
 							'due':1527763790,
 							'completed':false
 						},
 						{
 							'id':'e6dd2534-6879-4d1f-976e-c0831ab2ba6e',
-							'title':'bi-directional',
-							'description':'Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.',
+							'title':'Write first Hello World program',
+							'description':'Write "Hello World!" program to ensure that python is working correctly',
 							'created':1527745638,
 							'due':1527785772,
 							'completed':false
@@ -50,37 +50,37 @@ export default {
 				},
 				{
 					'id':'329d54d6-a5ac-4360-9d52-f0fe357da14e',
-					'title':'intranet',
+					'title':'Create first pyautogui bot',
 					'position':2,
 					'tasks':[
 						{
 							'id':'c7441ea1-ae73-48b4-a624-39020601f6c7',
-							'title':'functionalities',
-							'description':'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat.',
+							'title':'Install pyautogui on local machine',
+							'description':'Get pyautogui up and running. Read through tutorial to do this',
 							'created':1527782473,
 							'due':1527680255,
 							'completed':true
 						},
 						{
 							'id':'7a08ea70-8e6e-40f0-a621-77b9fd00d7f5',
-							'title':'Pre-emptive',
-							'description':'Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque.',
+							'title':'Test bot moving mouse',
+							'description':'Code algorithm to have the bot move with mouse',
 							'created':1527691441,
 							'due':1527775501,
 							'completed':false
 						},
 						{
 							'id':'c8bf6cbc-4c65-4351-a9fc-d58d4972b936',
-							'title':'modular',
-							'description':'Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+							'title':'Test bot typing keyboard',
+							'description':'Code algorithm to have the bot type with keyboard',
 							'created':1527728760,
 							'due':1527762375,
 							'completed':false
 						},
 						{
 							'id':'108800f1-8e2c-400f-a8ae-f19743b43822',
-							'title':'next generation',
-							'description':'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.',
+							'title':'Test both mouse and keyboard',
+							'description':'Code algorithm to have the bot both move the mouse and type with keyboard',
 							'created':1527660213,
 							'due':1527808807,
 							'completed':false
@@ -89,45 +89,45 @@ export default {
 				},
 				{
 					'id':'9e065122-15c8-4f62-b40b-04ee00615531',
-					'title':'matrix',
+					'title':'Setup bot for data entry',
 					'position':3,
 					'tasks':[
 						{
 							'id':'a7d06325-f743-4e53-a253-97819460cdbc',
-							'title':'Exclusive',
-							'description':'Morbi non quam nec dui luctus rutrum.',
+							'title':'Setup data entry environment for bot',
+							'description':'Setup excel spreadsheet test environment',
 							'created':1527691405,
 							'due':1527734121,
 							'completed':true
 						},
 						{
 							'id':'265779b4-3fe3-41ee-9496-8e4959b8d6f0',
-							'title':'empowering',
-							'description':'Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus.',
+							'title':'Have bot recognize buttons',
+							'description':'Take screencaps of buttons and have bot recgonize them',
 							'created':1527614020,
 							'due':1527606164,
 							'completed':false
 						},
 						{
 							'id':'3dd1872f-8ea8-4841-803d-5726b14a93de',
-							'title':'Front-line',
-							'description':'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue.',
+							'title':'Test bot with data entry',
+							'description':'Setup bot with mock data to test data entry',
 							'created':1527621758,
 							'due':1527758936,
 							'completed':false
 						},
 						{
 							'id':'8a594e2f-c521-42a7-a3b1-61947e97fbb8',
-							'title':'Customizable',
-							'description':'Proin eu mi. Nulla ac enim.',
+							'title':'Test bot with real data',
+							'description':'Monitor bot to see how it does with real data',
 							'created':1527691767,
 							'due':1527629177,
 							'completed':false
 						},
 						{
 							'id':'bf536de9-8f1f-4a1c-bce2-9dc1ddc0343e',
-							'title':'explicit',
-							'description':'Aliquam erat volutpat. In congue. Etiam justo.',
+							'title':'Deploy bot',
+							'description':'Deploy bot and have it automate various data entry tasks for the world',
 							'created':1527634768,
 							'due':1527774262,
 							'completed':true
@@ -138,123 +138,36 @@ export default {
 		},
 		{
 			'id':'f5e51cf9-a972-4374-a1b1-d787f75af229',
-			'title':'De-engineered',
+			'title':'Marketing data entry bot',
 			'created':1527728457,
 			'sections':[
 				{
 					'id':'5918009c-8133-4dd8-9f02-ede364ecd6e3',
-					'title':'parallelism',
+					'title':'Sales',
 					'position':1,
 					'tasks':[
 						{
 							'id':'d67c2f6f-0545-460a-a734-dc262907ae16',
-							'title':'intranet',
-							'description':'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo.',
+							'title':'Figure out how to sell bot',
+							'description':'',
 							'created':1527650648,
 							'due':1527791202,
-							'completed':false
-						},
-						{
-							'id':'bed0f9a1-0419-4cf7-b323-d63c79a0d16e',
-							'title':'motivating',
-							'description':'Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit.',
-							'created':1527731537,
-							'due':1527680253,
-							'completed':true
-						},
-						{
-							'id':'66f651e4-9194-4b7b-b9bf-3049e910e88b',
-							'title':'asymmetric',
-							'description':'Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.',
-							'created':1527698662,
-							'due':1527629588,
-							'completed':true
-						},
-						{
-							'id':'b798c9c1-ca1f-499b-ba76-f4e2633d61c3',
-							'title':'national',
-							'description':'Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque.',
-							'created':1527796190,
-							'due':1527632813,
-							'completed':false
-						}
-					]
-				},
-				{
-					'id':'cab7b632-b273-42f6-9583-651dc6f3e146',
-					'title':'encryption',
-					'position':2,
-					'tasks':[
-						{
-							'id':'c2eff27f-8c8b-40c2-97b1-2b5b428e1488',
-							'title':'high-level',
-							'description':'Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum.',
-							'created':1527630003,
-							'due':1527652279,
-							'completed':true
-						},
-						{
-							'id':'36194b5e-0816-460a-97e3-f263d04e64e4',
-							'title':'model',
-							'description':'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-							'created':1527611870,
-							'due':1527699267,
-							'completed':true
-						},
-						{
-							'id':'7ddc6079-f665-4691-beec-4d2dc65b2cac',
-							'title':'projection',
-							'description':'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis.',
-							'created':1527796548,
-							'due':1527633325,
 							'completed':false
 						}
 					]
 				},
 				{
 					'id':'f3c4e20b-d230-416d-9fdb-fec721feb873',
-					'title':'data-warehouse',
+					'title':'Advertising',
 					'position':3,
 					'tasks':[
 						{
 							'id':'a7ca331e-c3dd-479c-80e1-db87933cb987',
-							'title':'functionalities',
-							'description':'Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
+							'title':' ',
+							'description':' ',
 							'created':1527766724,
 							'due':1527649467,
 							'completed':false
-						},
-						{
-							'id':'40e7451a-4480-4c28-9607-448da34dfa61',
-							'title':'responsive',
-							'description':'Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
-							'created':1527661733,
-							'due':1527794276,
-							'completed':true
-						},
-						{
-							'id':'fbfd1cce-2140-45d2-bdc7-96e2bc4284ba',
-							'title':'User-friendly',
-							'description':'Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis.',
-							'created':1527751827,
-							'due':1527691929,
-							'completed':false
-						},
-						{
-							'id':'1b4b22d7-a1b5-43a8-b7a3-cb8693231242',
-							'title':'client-driven',
-							'description':'Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.',
-							'created':1527750602,
-							'due':1527776274,
-							'completed':true
-						},
-						{
-							'id':'281c1687-1a89-4a8c-afff-2a5f8bc71adc',
-							'title':'web-enabled',
-							'description':'Nunc rhoncus dui vel sem.',
-							'created':1527768820,
-							'due':1527735172,
-							'completed':true
 						}
 					]
 				}
