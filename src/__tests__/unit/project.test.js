92d9f2796e4b11878eae80a083016f4f86bf1120
hughxie <hughxie@outlook.com>
2018-07-22 14:20:32 -0400

    starting point for Project component unit testing

---
+++ src/__tests__/unit/project.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Project from '../../App/Home/Project/Project'
+
+describe('Project', () => {
+  it('should be defined', () => {
+    expect(Project).toBeDefined();
+  })
+})
