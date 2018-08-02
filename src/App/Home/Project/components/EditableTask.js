4940bd4e7d4bf61c918a32d5be0773f8354e5fcb
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-08-02 09:14:51 -0400

    Add: editable task is now a stateless component

--- src/App/Home/Project/components/EditableTask.js
+++ src/App/Home/Project/components/EditableTask.js
@@ -5,55 +5,54 @@ import EditableField from '../../../components/EditableField'
 import { deleteTask } from '../../../../graphql/mutations'
 import { graphql, compose } from 'react-apollo'
 
-class EditableTask extends Component {
-	close = () => {
+const EditableTask = (props) => {
+	const close = () => {
 		this.props.selectItem(null)
 	}
-	render() {
-		const trigger = (
-			<Icon name='ellipsis vertical'/>
-		)
-		
-		const options = [
-			{ key: 'trash', text: 'Delete', icon: 'trash', value: 'delete' }
-		]
 
-		const handleOptions = async (e, { value }) => {
-			if(value === 'delete'){
-				await this.props.deleteTask({
-					variables: {
-						id: this.props.item.data.id
-					}
-				})
-				this.props.selectItem(null)
-			}
+	const trigger = (
+		<Icon name='ellipsis vertical'/>
+	)
+	
+	const options = [
+		{ key: 'trash', text: 'Delete', icon: 'trash', value: 'delete' }
+	]
+
+	const handleOptions = async (e, { value }) => {
+		if(value === 'delete'){
+			await this.props.deleteTask({
+				variables: {
+					id: this.props.item.data.id
+				}
+			})
+			this.props.selectItem(null)
 		}
-		
-    return (
-			<Column>
-				<Row className='justify-between'>
-					<Icon name='close' onClick={this.close}/>
-					<Dropdown trigger={trigger} options={options} onChange={handleOptions} pointing='top right' icon={null} />
-				</Row>
-        <EditableField
-					classes='f1 lh-copy tl nowrap'
-					placeholder='Task Title'
-          value={this.props.item.data.title}
-          onChange={this.props.onChange}
-					field='title'
-        />
-        {this.props.item.data.description !== null &&
-        <EditableField
-					classes='ba b--light-gray pa2 ma2 tl'
-					placeholder='Write task description here...'
-					value={this.props.item.data.description}
-					onChange={this.props.onChange}
-					field='description'
-        />
-        }
-      </Column>
-    )
 	}
+	
+	return (
+		<Column>
+			<Row className='justify-between'>
+				<Icon name='close' onClick={close}/>
+				<Dropdown trigger={trigger} options={options} onChange={handleOptions} pointing='top right' icon={null} />
+			</Row>
+			<EditableField
+				classes='f1 lh-copy tl nowrap'
+				placeholder='Task Title'
+				value={props.item.data.title}
+				onChange={props.onChange}
+				field='title'
+			/>
+			{props.item.data.description !== null &&
+			<EditableField
+				classes='ba b--light-gray pa2 ma2 tl'
+				placeholder='Write task description here...'
+				value={props.item.data.description}
+				onChange={props.onChange}
+				field='description'
+			/>
+			}
+		</Column>
+	)
 }
 
 export default compose(
