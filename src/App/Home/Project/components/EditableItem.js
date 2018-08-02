fef797b71523324f8c548f29d22ea70163b62338
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-08-02 09:05:44 -0400

    Add: editable item to stateless component

--- src/App/Home/Project/components/EditableItem.js
+++ src/App/Home/Project/components/EditableItem.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react'
 import { Row } from '../../../../utils/anvil'
 import EditableField from '../../../components/EditableField';
 
-class EditableItem extends Component {
-	render() {
-    return (
-      <Row>
-        <EditableField
-					field='title'
-					classes='b'
-          value={this.props.item.data.title}
+const EditableItem = (props) => (
+	<Row>
+			<EditableField
+				field='title'
+				classes='b'
+				value={props.item.data.title}
+				onChange={props.onChange}
+			/>
+			{/* // Not Showing description in item list 
+			{this.props.item.data.description &&
+				<EditableField
+					classes='mh2'
+					field='description'
+					value={this.props.item.data.description}
 					onChange={this.props.onChange}
 					
-        />
-				{/* // Not Showing description in item list 
-				{this.props.item.data.description &&
-					<EditableField
-						classes='mh2'
-						field='description'
-						value={this.props.item.data.description}
-						onChange={this.props.onChange}
-						
-					/>
-				} */}
-      </Row>
-    )
-	}
-}
+				/>
+			} */}
+	</Row>
+)
+
 export default EditableItem
