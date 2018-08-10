e30f1bf8b5b7f4460b60b324b24f815916c0b123
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-08-10 18:26:13 -0400

    Remove: unused styled components

--- src/App/Home/components/topbar.js
+++ src/App/Home/components/topbar.js
@@ -28,20 +28,3 @@ const Topbar = (props) => {
 }
 
 export default Topbar
-
-const Logo = styled.img`
-	width: 12rem;
-    height: 12rem;
-`
-
-const Container = styled(Segment)`
-	display: flex;
-	justify-content: space-between;
-	width: 100%;
-	padding: 0.2rem;
-	p {
-		font-weight: 800;
-		font-size: 2rem;
-		margin: 0;
-	}
-`
